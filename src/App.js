b9bdcd7272fedc30c8480ee219fa877f84c3ac7b
Saket Nandanwar <70093885+whodissaket@users.noreply.github.com>
2021-11-15 13:25:22 +0530

    Responsive Navbar , Added: Floating Whatsapp button

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import FloatingWhatsApp from "react-floating-whatsapp";
 
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Mainpage from "./components/Landing/Mainpage";
 import ContactUs from "./components/ContactUs/ContactUs";
 import Faq from "./components/Faqs/Faq";
-import About from "./components/About/About"
+import About from "./components/About/About";
 import Gallery from "./components/Gallery/Gallery";
-import Services from "./components/SectionServices/Services"
-import PaintQuotation from "./components/SectionServices/PaintQuotation/PaintQuotation"
-import InteriorPainting from "./components/SectionServices/InteriorPainting/InteriorPainting"
-import MetalPainting from "./components/SectionServices/MetalPainting/MetalPainting"
-import ExteriorPainting from "./components/SectionServices/ExteriorPainting/ExteriorPainting"
-import Waterproofing from "./components/SectionServices/Waterproofing/Waterproofing"
+import Services from "./components/SectionServices/Services";
+import PaintQuotation from "./components/SectionServices/PaintQuotation/PaintQuotation";
+import InteriorPainting from "./components/SectionServices/InteriorPainting/InteriorPainting";
+import MetalPainting from "./components/SectionServices/MetalPainting/MetalPainting";
+import ExteriorPainting from "./components/SectionServices/ExteriorPainting/ExteriorPainting";
+import Waterproofing from "./components/SectionServices/Waterproofing/Waterproofing";
 import SimpleReactLightbox from "simple-react-lightbox";
 
 function App() {
-
-
   return (
     <div className="App">
-      {/* <Navbar /> */}
       {/* Navbar Routing */}
       <Router>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Mainpage />
@@ -50,7 +49,15 @@ function App() {
           {/* Service page routing ends */}
         </Switch>
       </Router>
-    </div >
+      <FloatingWhatsApp
+        phoneNumber="123456789"
+        accountName="Sensei"
+        avatar="https://avatarfiles.alphacoders.com/262/thumb-1920-262215.png"
+        allowclickaway
+        notification
+        notificationdelay={5000}
+      />
+    </div>
   );
 }
 
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: "Interior Painting",
+    path: "/interiorpainting",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Exterior Painting",
+    path: "/exteriorpainting",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Waterproofing",
+    path: "/waterproofing",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Metal Painting",
+    path: "/metalpainting",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Paint Quotation",
+    path: "/paintquotation",
+    cName: "dropdown-link",
+  },
+];
